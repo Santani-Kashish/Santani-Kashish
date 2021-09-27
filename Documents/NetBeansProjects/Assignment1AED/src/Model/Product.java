2c401f5aa3ef1a330288181b5c0cef7d9ac924a0
Santani-Kashish <santani.k@northeastern.edu>
2021-09-26 23:06:59 -0400

    Comminting sample

---
+++ Documents/NetBeansProjects/Assignment1AED/src/Model/Product.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.io.File;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author kashi
+ */
+public class Product {
+    private String Name;
+    private String GeographicData;
+    private String DateofBirth;
+    private String Telephone;
+    private String FAX;
+    private String Email;
+    private String SSN;
+    private String MedicalRecord;
+    private String HealthNumber;
+    private String BankAccount;
+    private String LicenseNumber;
+    private String Vehicleidentifier;
+    private String Deviceidentifier;
+    private String Linkedin;
+    private String Internetprotocol;
+    private String uniqueidentifier;
+    
+    private ImageIcon img;
+    public Object set;
+
+    public ImageIcon getImg() {
+        return img;
+    }
+
+    public void setImg(ImageIcon img) {
+        this.img = img;
+    }
+    
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getGeographicData() {
+        return GeographicData;
+    }
+
+    public void setGeographicData(String GeographicData) {
+        this.GeographicData = GeographicData;
+    }
+
+    public String getDateofBirth() {
+        return DateofBirth;
+    }
+
+    public void setDateofBirth(String DateofBirth) {
+        this.DateofBirth = DateofBirth;
+    }
+
+    public String getTelephone() {
+        return Telephone;
+    }
+
+    public void setTelephone(String Telephone) {
+        this.Telephone = Telephone;
+    }
+
+    public String getFAX() {
+        return FAX;
+    }
+
+    public void setFAX(String FAX) {
+        this.FAX = FAX;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getMedicalRecord() {
+        return MedicalRecord;
+    }
+
+    public void setMedicalRecord(String MedicalRecord) {
+        this.MedicalRecord = MedicalRecord;
+    }
+
+    public String getHealthNumber() {
+        return HealthNumber;
+    }
+
+    public void setHealthNumber(String HealthNumber) {
+        this.HealthNumber = HealthNumber;
+    }
+
+    public String getBankAccount() {
+        return BankAccount;
+    }
+
+    public void setBankAccount(String BankAccount) {
+        this.BankAccount = BankAccount;
+    }
+
+    public String getLicenseNumber() {
+        return LicenseNumber;
+    }
+
+    public void setLicenseNumber(String LicenseNumber) {
+        this.LicenseNumber = LicenseNumber;
+    }
+
+    public String getVehicleidentifier() {
+        return Vehicleidentifier;
+    }
+
+    public void setVehicleidentifier(String Vehicleidentifier) {
+        this.Vehicleidentifier = Vehicleidentifier;
+    }
+
+    public String getDeviceidentifier() {
+        return Deviceidentifier;
+    }
+
+    public void setDeviceidentifier(String Deviceidentifier) {
+        this.Deviceidentifier = Deviceidentifier;
+    }
+
+    public String getLinkedin() {
+        return Linkedin;
+    }
+
+    public void setLinkedin(String Linkedin) {
+        this.Linkedin = Linkedin;
+    }
+
+    public String getInternetprotocol() {
+        return Internetprotocol;
+    }
+
+    public void setInternetprotocol(String Internetprotocol) {
+        this.Internetprotocol = Internetprotocol;
+    }
+
+    public String getUniqueidentifier() {
+        return uniqueidentifier;
+    }
+
+    public void setUniqueidentifier(String uniqueidentifier) {
+        this.uniqueidentifier = uniqueidentifier;
+    }
+
+    public void setlinkedin(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
